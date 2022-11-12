880bfa9be41f8c089b8c3974622974542cc891bc
HendSaeed025 <118080130+HendSaeed025@users.noreply.github.com>
2022-11-12 23:08:56 +0200

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,21 @@
+myfruitlist = ["apple", "banana","cherry"]
+print(myfruitlist)
+print(type(myfruitlist))
+print(myfruitlist[0])
+print(myfruitlist[1])
+print(myfruitlist[2])
+myfruitlist[2] = "orange"
+print(myfruitlist)
+myfinalanswertuple = ("apple","banana","pineapple")
+print(myfinalanswertuple)
+print(type(myfinalanswertuple))
+print(myfinalanswertuple[1])
+print(myfinalanswertuple[2])
+myfavoritefruitdictinary={
+    "akua":"apple",
+    "saanvi":"banana",
+    "paulo":"pineapple"
+}
+print(myfavoritefruitdictinary)
+print(type(myfavoritefruitdictinary))
+print (myfavoritefruitdictinary["saanvi"])
---
+++ conditionals.py
@@ -0,0 +1,3 @@
+userReply=input("Do you need to ship a Package?(enter yes or no)")
+if userReply =="yes":
+    print("we can help you ship that package!")
